20b2f0fd484404b20227b388c6dcf5c611f53a68
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-04-03 10:32:19 +0530

    init: Initialise Project For Practices

---
+++ 03(D)-ui-navigation-controller-TE-T470/03(D)-ui-navigation-controller-TE-T470/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  03(D)-ui-navigation-controller-TE-T470
+//
+//  Created by Bhumik Poshiya on 03/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
