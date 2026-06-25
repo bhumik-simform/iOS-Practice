b931d165d9681dc0a5a04b26eb308628474334d8
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-06-25 14:57:58 +0530

    init: Initialise project for practice TE-T709 mvc architecture.

---
+++ 05-ios-architectures/05-mvc-architecture/05-mvc-architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  05-mvc-architecture
+//
+//  Created by Bhumik Poshiya on 25/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
